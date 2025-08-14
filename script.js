e92d785a9a6cb2607f83081c53e354aa52d32132
AnyDytKep <kepodech121@gmail.com>
2025-08-14 12:08:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage(item) {
+    const message = document.getElementById('message');
+    message.innerHTML = `Kamu tertarik dengan ${item}? Hubungi: <a href="https://t.me/ariks_ta" style="color:#00f">t.me/ariks_ta</a>`;
+}
